0b7388af25b46383debbcfa05b8942d27e7c2888
unknown <mark.p.crowley@mycit.ie>
2018-11-24 23:48:30 +0000

    Restarting

--- routes/contact.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//- get contact page
-router.get('/', function(req, res, next) {
-  res.render('contact');
-});
-
-module.exports = router;
