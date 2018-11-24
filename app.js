3af33e3be5d47193c313fec9436fa5c5a9b2694f
markCIT2018 <mark.p.crowley@mycit.ie>
2018-11-24 18:35:41 +0000

    continued development, added some pug files/updates and various other dependencies

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 //-- added variables 
 var session = require('express-session');
 var passport = require('passport');
+var ExpressValidator = require ('express-validator');
 var LocalStrategy = require('passport-local').Strategy;
 var mutler = require('multer');
+var upload = mutler({dest:'./uploads'});
 var flash = require('connect-flash');
 var mongoose = require('mongoose');
 var mongo = require('mongodb');
@@ -16,6 +18,8 @@ var db = mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
@@ -29,8 +33,55 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// handle sessions
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+
+}));
+
+// passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//validator
+app.use(ExpressValidator());
+app.use(express.json());
+app.post('/user', (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(user => res.json(user));
+});
+
+// ...rest of the initial code omitted for simplicity.
+const { check, validationResult } = require('express-validator/check');
+
+app.post('/user', [
+  // username must be an email
+  check('username').isEmail(),
+  // password must be at least 5 chars long
+  check('password').isLength({ min: 5 })
+], (req, res) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(user => res.json(user));
+});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//- new page which says something from tutorial, meaningless,can be deleted.
+router.get('/cool', function(req, res, next) {
+  res.send('Damn, you\'re looking good.');
+});
+
+
+
+
 module.exports = router;
